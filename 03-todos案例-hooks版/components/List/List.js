3af50fd8c47d9b2fa4a92a9bd837ffc9adf44ca4
luoxiuwen8679 <1696674898@qq.com>
2021-07-27 00:27:00 +0800

    router

--- 03-todos案例-hooks版/components/List/List.js
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-
-import './List.css'
-export default function List(props) {
-  return <ul className="todo-main">{props.children}</ul>
-}
--- src1/utils/utils.js
+++
@@ -1,5 +0,0 @@
-function sels(data){
-    data=JSON.stringify(data)
-    localStorage.setItem('test',data)
-}
-export {sels}
